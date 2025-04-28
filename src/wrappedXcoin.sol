adc9cbc04097a75670c5a9d7790baadc188087e4
Ankit Lingwal <ankitlingwal9@gmail.com>
2025-04-28 12:03:19 +0530

    Contracts setup

---
+++ src/wrappedXcoin.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract WXcoin is ERC20 {
+    constructor() ERC20("WXcoin" , "WXC"){
+        
+    }
+}
+
