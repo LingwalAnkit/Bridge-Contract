e7e9d9e3a2e6d9e07bcd043e16f30559e0471fa3
Ankit Lingwal <ankitlingwal9@gmail.com>
2025-06-18 13:40:16 +0530

    understanding locking a contract

---
+++ src/lockXcoin.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract LockXcoin {
+    constructor() {
+
+    }
+
+    function Deposite() public {
+
+    }
+
+    function Withdraw() public {
+
+    }
+}
